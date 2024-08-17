20f98a80e366852052da9fe36f7793521be97ae5
cottons-kr <devkim@hanarin.uk>
2024-08-17 19:11:28 +0000

    chore: Update Typography component to use framer-motion for animation

--- components/ui/Typography/Base.tsx
+++ components/ui/Typography/Base.tsx
@@ -1,6 +1,8 @@
-import { createElement } from 'react'
-import { BaseProps, TypographyTag, TypographyVariant } from './shared'
+'use client'
+
+import { BaseProps, TypographyVariant } from './shared'
 import classNames from 'classnames'
+import { motion } from 'framer-motion'
 
 import s from './style.module.scss'
 
@@ -9,23 +11,19 @@ type BaseTypographyProps = BaseProps & {
 }
 export function BaseTypography(props: BaseTypographyProps) {
   const { variant, weight, size, color } = props
-  const tag = TypographyTag[variant]
 
-  return createElement(
-    tag,
-    {
-      children: props.children,
-      style: {
-        color: color ?
-          `var(${color})` :
-          undefined
-      },
-      className: classNames(
-        s[variant],
-        weight && s[weight],
-        size && s[size],
-        props.className,
-      ),
-    }
-  )
+  return <motion.p
+    children={props.children}
+    style={{
+      color: color ?
+        `var(${color})` :
+        undefined
+    }}
+    className={classNames(
+      s[variant],
+      weight && s[weight],
+      size && s[size],
+      props.className,
+    )}
+  />
 }
