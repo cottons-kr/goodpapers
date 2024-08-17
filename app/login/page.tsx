753a26952c8d4d0b095c6695ce2e0bbe0fd0cfe3
cottons-kr <devkim@hanarin.uk>
2024-08-17 14:20:23 +0000

    chore: Update gitignore and add authentication route

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,15 +1,35 @@
 import Flex from '@/components/layout/Flex'
 import Button from '@/components/ui/Button'
 import { ButtonSize, ButtonVariant } from '@/components/ui/Button/shared'
+import Image from 'next/image'
+import Logo from '@/public/logo.png'
+import { auth, signIn } from '@/utils/auth'
+import { redirect } from 'next/navigation'
 
-export default function LoginPage() {
+import s from './page.module.scss'
+
+export default async function LoginPage() {
+  const session = await auth()
+  if (session?.user) {
+    redirect('/')
+  }
+
+  const handleLogin = async () => {
+    'use server'
+    await signIn('google', { redirectTo: '/' })
+  }
+
   return <>
-    <Flex direction='column'>
-      <Button
-        fullWidth
-        variant={ButtonVariant.PRIMARY}
-        size={ButtonSize.LARGE}
-      >Google로 로그인</Button>
+    <Flex className={s.container} direction='column' align='center' justify='space-between' height='100%'>
+      <Image className={s.logo} src={Logo} alt='굿페이퍼즈' />
+      <form className={s.login} action={handleLogin}>
+        <Button
+          type='submit'
+          fullWidth
+          variant={ButtonVariant.PRIMARY}
+          size={ButtonSize.LARGE}
+        >Google로 로그인</Button>
+      </form>
     </Flex>
   </>
 }
